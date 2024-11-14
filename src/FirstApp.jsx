9318039b28d5e97a23948336f44a128df73f0cd6
Juan Manuel <juanmanuelmaya046@gmail.com>
2024-11-13 22:01:10 -0500

    Finalizacion seccion 4: Primeros Pasos en React useState

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,20 +1,36 @@
-const saludo = () => {
+/* const saludo = () => {
   return <h1>desde una funcion</h1>;
-};
+}; */
 
-export const FirstApp = (props) => {
-  const nombre = "Juan Manuel !!";
-  console.log(props);
+import PropTypes from 'prop-types'
 
+export const FirstApp = ({title,subTitle, name})=> {
+  /* const nombre = "Juan Manuel !!"; */
+  console.log('salen props1',title);
   return (
     <>
-      <h1>Nombre: {nombre}</h1>
+      <h1>{title}</h1>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque at
+        Lorem ipsum dolor, sit amet consectet ur adipisicing elit. Eaque at
         accusantium sapiente iusto quas voluptates perspiciatis, ab perferendis
         numquam officia. Tenetur sed perspiciatis exercitationem dignissimos
-        dolores autem laborum aspernatur vitae!
+        dolores autem laborum aspernatur vitae! {subTitle} mi {name}
       </p>
     </>
   );
 };
+
+
+/* propTypes --> Define el tipo de dato que va a recibir el componente */
+FirstApp.propTypes={
+  title:PropTypes.string.isRequired,
+  subTitle:PropTypes.string,
+  name: PropTypes.string
+}
+
+/* Default props --> define un valor por defecto a las props */
+FirstApp.defaultProps={
+  title:'No hay titulo',
+  subTitle:'No hay subtitulo',
+  name:"Juan Manuel"
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelloWorldApp } from "./HelloWorldApp";
-import { FirstApp } from "./FirstApp";
+//import { HelloWorldApp } from "./HelloWorldApp";
+//import { FirstApp } from "./FirstApp";
+import CounterApp from "./CounterApp";
+
 
 import "./style.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp />
+    {/* <FirstApp  title="Hola soy, Goku" /> */}
+    <CounterApp value={2}/>
   </React.StrictMode>
 );
